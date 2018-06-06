0030ce72e316baf623e7c5ff682c5058be4e5c86
Magnus Lång <magnus.lang@it.uu.se>
2018-06-06 21:57:11 +0200

    SmtlibSatSolver: Add flag for encoding with bitvectors
    A new flag, --smtlib-bv, switches the encoding to use bitvectors. This
    allows the use of bitvector-only SMT solvers such as boolector and STP.
    
    STP does not yet work, though, as it does not support
    "(get-value (...))", only "(get-model)", and also cannot parse the
    "(set-option :produce-models true)" directive, which is required by some
    other solvers.

--- src/SmtlibSatSolver.cpp
+++ src/SmtlibSatSolver.cpp
@@ -21,16 +21,23 @@
 
 #include <llvm/Support/CommandLine.h>
 #include <boost/variant.hpp>
+#include <iomanip>
 
-static llvm::cl::opt<std::string> cl_cmd("smtlib-cmd",llvm::cl::NotHidden,
-                                         llvm::cl::init("z3 -in"),
-                                         llvm::cl::desc("The SMTLib-compatible solver command."));
+static llvm::cl::opt<bool>
+cl_bv("smtlib-bv",llvm::cl::NotHidden,
+      llvm::cl::desc("Encode integers as bitvectors in SMTLib."));
+
+static llvm::cl::opt<std::string>
+cl_cmd("smtlib-cmd",llvm::cl::NotHidden,
+       llvm::cl::init("z3 -in"),
+       llvm::cl::desc("The SMTLib-compatible solver command."));
 
 SmtlibSatSolver::SmtlibSatSolver()
   : out(), in(),
     z3(std::string(cl_cmd), boost::process::std_out > out, boost::process::std_in < in) {
   in << "(set-option :produce-models true)\n";
-  in << "(set-logic QF_IDL)\n";
+  if (cl_bv) in << "(set-logic QF_BV)\n";
+  else in << "(set-logic QF_IDL)\n";
 }
 
 SmtlibSatSolver::~SmtlibSatSolver() {}
@@ -40,12 +47,34 @@ void SmtlibSatSolver::reset() {
   in << "(reset)\n";
 }
 
+static unsigned ilog2(unsigned v) {
+  const unsigned int b[] = {0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000};
+  const unsigned int S[] = {1, 2, 4, 8, 16};
+  int i;
+
+  unsigned int r = 0; // result of log2(v) will go here
+  for (i = 4; i >= 0; i--) { // unroll for speed...
+    if (v & b[i]) {
+      v >>= S[i];
+      r |= S[i];
+    }
+  }
+  return r;
+}
+
 void SmtlibSatSolver::alloc_variables(unsigned count) {
   no_vars = count;
+  unsigned hexdigs = ilog2(count-2)/4+1;
   for (unsigned i = 0; i < no_vars; ++i) {
-    in << "(declare-fun e" << i << " () Int)\n";
-    in << "(assert (>= e" << i << " 0))\n";
-    in << "(assert (< e" << i << " " << no_vars << "))\n";
+    if (cl_bv) {
+      in << "(declare-fun e" << i << " () (_ BitVec " << (hexdigs*4) << "))\n";
+      in << "(assert (bvult e" << i << " #x"
+         << std::setfill('0') << std::setw(hexdigs) << std::hex << no_vars << std::dec << "))\n";
+    } else {
+      in << "(declare-fun e" << i << " () Int)\n";
+      in << "(assert (>= e" << i << " 0))\n";
+      in << "(assert (< e" << i << " " << no_vars << "))\n";
+    }
   }
 
   in << "(assert (distinct ";
@@ -56,13 +85,16 @@ void SmtlibSatSolver::alloc_variables(unsigned count) {
 }
 
 void SmtlibSatSolver::add_edge(unsigned from, unsigned to) {
-  in << "(assert (< e" << from << " e" << to << "))\n";
+  if (cl_bv) in << "(assert (bvult e" << from << " e" << to << "))\n";
+  else in << "(assert (< e" << from << " e" << to << "))\n";
 }
 
 void SmtlibSatSolver::add_edge_disj(unsigned froma, unsigned toa,
                                     unsigned fromb, unsigned tob) {
-  in << "(assert (or (< e" << froma << " e" << toa << ")"
-    "(< e" << fromb << " e" << tob << ")))\n";
+  if (cl_bv) in << "(assert (or (bvult e" << froma << " e" << toa << ")"
+               "(bvult e" << fromb << " e" << tob << ")))\n";
+  else in << "(assert (or (< e" << froma << " e" << toa << ")"
+         "(< e" << fromb << " e" << tob << ")))\n";
 }
 
 bool SmtlibSatSolver::check_sat() {
@@ -173,8 +205,17 @@ std::vector<unsigned> SmtlibSatSolver::get_model() {
     assert(e.kind() == SExpr::LIST);
     const auto &l2 = e.list().elems;
     assert(l2.size() == 2);
-    assert(l2[1].kind() == SExpr::INT);
-    res[i] = l2[1].num().value;
+    if (cl_bv) {
+      assert(l2[1].kind() == SExpr::TOKEN);
+      const std::string &t = l2[1].token().name;
+      assert(t.size() > 2 && t[0] == '#' && t[1] == 'x');
+      char *end;
+      res[i] = std::strtol(t.c_str()+2, &end, 16);
+      assert((end - t.c_str()) == long(t.size()));
+  } else {
+      assert(l2[1].kind() == SExpr::INT);
+      res[i] = l2[1].num().value;
+    }
   }
 
   return res;
