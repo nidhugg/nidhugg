cf9755cb0156aa1702d99fd02a6b752e5c491edf
Magnus Lång <magnus.lang@it.uu.se>
2018-06-30 18:51:27 +0200

    test-swsc.py: Exit early with nonzero on failure

--- tests/litmus/test-swsc.py
+++ tests/litmus/test-swsc.py
@@ -53,23 +53,21 @@ def res_to_string(tst, res):
             s = s + tst['failure']
         else:
             s = s + '(unknown)'
-        return s
+        return (s, False)
 
     if res['allow'] != tst['expect allow']:
         s = s + bcolors.FAIL + ' FAILURE: unexpected result ' + ('Allow' if tst['nidhugg allow'] else 'Forbid') + bcolors.ENDC
-        return s
+        return (s, False)
     else:
         s = s + ' ' + ('Allow' if res['allow'] else 'Forbid')
 
     if tst['expected trace count'] != res['tracecount']:
-    	s = s + bcolors.FAIL + ' FAILURE: not same trace as ' + str(tst['expected trace count']) + bcolors.ENDC
+        return (s + bcolors.FAIL + ' FAILURE: not same trace as ' + str(tst['expected trace count']) + bcolors.ENDC, False)
 
-    s = s + bcolors.OKGREEN + ' OK ' + bcolors.ENDC + ' : ' + str(res['tracecount'])
-
-    return s
+    return (s + bcolors.OKGREEN + ' OK ' + bcolors.ENDC + ' : ' + str(res['tracecount']), True)
 
     
-def runallswsc(jobs):
+def runallswsc(jobs, keep_going):
     logfile = open(OUTPUTTFILE, 'w')
     logfile.write('# Results of running swsc to all C tests.\n')
     version = subprocess.check_output([SWSCBIN, '--version'], stderr = subprocess.STDOUT).decode()
@@ -78,7 +76,8 @@ def runallswsc(jobs):
     logfile.write('# The tests where executed using test-swsc.py.\n')
     logfile.write('# Date: ' + datetime.datetime.now().strftime('%y%m%d-%H:%M')+'\n')
     logfile.write('\n')
-    
+
+    ret = 0
     totaltracecount = 0
     tests = get_expected(LISTFILE)
     initial_count = len(tests)
@@ -110,14 +109,20 @@ def runallswsc(jobs):
         (tst,w,p) = work.pop(done_n)
         totaltracecount += res['tracecount']
         print('{0:4}: '.format(done_n), end = '')
-        print(res_to_string(tst, res))
-        logfile.write(res_to_string(tst, res)  + '\n')
+        (s, ok) = res_to_string(tst, res)
+        print(s)
+        logfile.write(s  + '\n')
+        if not ok:
+            if not keep_going: tests.clear()
+            ret = 1
         give_work(w, p)
 
     runtime = time.time() - t0
     logfile.write('# Total number of traces: ' + str(totaltracecount) + '\n')
     logfile.write('# Total running time: {0:.2f} s\n'.format(runtime))
     logfile.close()
+
+    return ret
 
 def slave(q, p):
     while True:
@@ -147,19 +152,15 @@ def run_test(tst):
     return res
 
 def runoneswsc(filename):
-    found = False
-
     for tst in get_expected(LISTFILE):
-        if tst['tstname'] != filename:
-            continue
-        else:
-            found = True
+        if tst['tstname'] == filename:
             res = run_test(tst)
-            print(res_to_string(tst, res))
-            break
+            (s, ok) = res_to_string(tst, res)
+            print(s)
+            return 0 if ok else 1
 
-    if found == False:
-        print('Test case was not found!')
+    print('Test case was not found!')
+    return 2
 
 ####################################################
 
@@ -174,16 +175,19 @@ if __name__ == "__main__":
             threads = "1"
     parser.add_argument('-j', dest='jobs', metavar='N', type=int,
                         default=int(threads), help='Number or parallel jobs')
+    parser.add_argument('-k', '--keep-going', dest='keep_going',
+                        action='store_true',
+                        help='Run all tests, even if one fails')
     modes = parser.add_subparsers(metavar='mode',dest='mode')
     all_parser = modes.add_parser('all', help='Run all tests')
     one_parser = modes.add_parser('one', help='Run single test')
     one_parser.add_argument('test', help='testname(without_dot_c)');
     args = parser.parse_args()
     if (args.mode == "all"):
-        runallswsc(args.jobs)
-        sys.exit(0)
+        ret = runallswsc(args.jobs, args.keep_going)
+        sys.exit(ret)
     elif (args.mode == "one"):
-        runoneswsc(args.test)
-        sys.exit(0)
+        ret = runoneswsc(args.test)
+        sys.exit(ret)
     else:
         parser.print_help()
