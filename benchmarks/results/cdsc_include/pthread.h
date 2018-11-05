8216960dcaa69b9946b9a981f3bddd77ddd318c8
Magnus Lång <magnus.lang@it.uu.se>
2018-11-05 15:15:17 +0100

    Add floating_read and lastwrite benchmarks

--- benchmarks/results/cdsc_include/pthread.h
+++ benchmarks/results/cdsc_include/pthread.h
@@ -6,3 +6,8 @@ static inline int pthread_create(pthread_t *t, pthread_attr_t *attr,
                                  void *(*s)(void*), void* a) {
   return thrd_create(t, (thrd_start_t)s, a);
 }
+
+static inline int pthread_join(pthread_t thread, void **retval) {
+  /* Not C11, just CDSChecker */
+  return thrd_join(thread);
+}
