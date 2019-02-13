946ae2da5b027b765d479277c9ec501402ece7ba
Magnus Lång <magnus.lang@it.uu.se>
2019-02-13 15:00:44 +0100

    Only give "not supported" warnings for --wsc
    So that you can still run source&family on all benchmarks

--- src/Execution.cpp
+++ src/Execution.cpp
@@ -3521,8 +3521,9 @@ void Interpreter::run() {
     }
   }
   CurrentThread = 0;
-  if (!std::any_of(Threads.begin(), Threads.end(),
-                   [](const Thread &T) { return T.AssumeBlocked; })) {
+  if (conf.memory_model == Configuration::WEAK_SC
+      && !std::any_of(Threads.begin(), Threads.end(),
+                      [](const Thread &T) { return T.AssumeBlocked; })) {
     for (const Thread &T : Threads) {
       if (!T.ECStack.empty()) {
         llvm::dbgs() << "Deadlocks not supported\n";
