97c57c124de51e51c2fa18047488f7952ae72224
Magnus Lång <magnus.lang@it.uu.se>
2019-09-04 14:57:55 +0200

    Revert "Make -wsc the default mode"
    This reverts commit a05dcf1686e5a4460620f51edf6c7036b99edba4.

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -48,7 +48,7 @@ cl_max_search_depth("max-search-depth",
                     llvm::cl::desc("Bound the length of the analysed computations (# instructions/events per process)"));
 
 static llvm::cl::opt<Configuration::MemoryModel>
-cl_memory_model(llvm::cl::NotHidden, llvm::cl::init(Configuration::WEAK_SC),
+cl_memory_model(llvm::cl::NotHidden, llvm::cl::init(Configuration::MM_UNDEF),
                 llvm::cl::desc("Select memory model"),
                 llvm::cl::values(clEnumValN(Configuration::SC,"sc","Sequential Consistency"),
                                  clEnumValN(Configuration::WEAK_SC,"wsc","Weak Sequential Consistency"),
