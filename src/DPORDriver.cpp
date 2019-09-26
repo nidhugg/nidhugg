nidhugg/nidhugg

commit 22cd13b0b83abb7a0e92424f03f0a405ba27fc1c

Separately count executions aborted by assume(false)
Trace count will now be subdivided into consistent traces, and
assume-blocked traces. The output format for trace counts at the end of
exploration is changed to print each class on a separate line, easing
readability for humans and parsability for scripts.

Introduce new common base class DPORInterpreter for all common
functionality of interpreters used.

src/DPORDriver.cpp
@@ -101,9 +101,10 @@ void DPORDriver::reparse(){
   }
 }
 
-llvm::ExecutionEngine *DPORDriver::create_execution_engine(TraceBuilder &TB, const Configuration &conf) const {
+std::unique_ptr<DPORInterpreter> DPORDriver::
+create_execution_engine(TraceBuilder &TB, const Configuration &conf) const {
   std::string ErrorMsg;
-  llvm::ExecutionEngine *EE = 0;
+  std::unique_ptr<DPORInterpreter> EE = 0;
   switch(conf.memory_model){
   case Configuration::SC:
     EE = llvm::Interpreter::create(mod,static_cast<TSOPSOTraceBuilder&>(TB),conf,&ErrorMsg);
@@ -150,8 +151,8 @@ llvm::ExecutionEngine *DPORDriver::create_execution_engine(TraceBuilder &TB, con
   return EE;
 }
 
-Trace *DPORDriver::run_once(TraceBuilder &TB) const{
-  std::shared_ptr<llvm::ExecutionEngine> EE(create_execution_engine(TB,conf));
+Trace *DPORDriver::run_once(TraceBuilder &TB, bool &assume_blocked) const{
+  std::unique_ptr<DPORInterpreter> EE(create_execution_engine(TB,conf));
 
   // Run main.
   EE->runFunctionAsMain(mod->getFunction("main"), conf.argv, 0);
@@ -163,6 +164,8 @@ Trace *DPORDriver::run_once(TraceBuilder &TB) const{
     static_cast<llvm::Interpreter*>(EE.get())->checkForCycles();
   }
 
+  assume_blocked = EE->assumeBlocked();
+
   EE.reset();
 
   Trace *t = 0;
@@ -173,14 +176,13 @@ Trace *DPORDriver::run_once(TraceBuilder &TB) const{
       static_cast<POWERARMTraceBuilder&>(TB).replay();
       Configuration conf2(conf);
       conf2.ee_store_trace = true;
-      llvm::ExecutionEngine *E = create_execution_engine(TB,conf2);
+      std::unique_ptr<DPORInterpreter> E = create_execution_engine(TB,conf2);
       E->runFunctionAsMain(mod->getFunction("main"), conf.argv, 0);
       E->runStaticConstructorsDestructors(true);
       if(conf.check_robustness){
-        static_cast<llvm::Interpreter*>(E)->checkForCycles();
+        static_cast<llvm::Interpreter*>(E.get())->checkForCycles();
       }
       t = TB.get_trace();
-      delete E;
     }else{
       t = TB.get_trace();
     }
@@ -244,16 +246,18 @@ DPORDriver::Result DPORDriver::run(){
     if((computation_count+1) % 1000 == 0){
       reparse();
     }
-    Trace *t = run_once(*TB);
-    bool t_used = false;
+    bool t_used, assume_blocked = false;
+    Trace *t = run_once(*TB, assume_blocked);
     if(t && conf.debug_collect_all_traces){
       res.all_traces.push_back(t);
       t_used = true;
     }
-    if(TB->sleepset_is_empty()){
-      ++res.trace_count;
-    }else{
+    if(!TB->sleepset_is_empty()) {
       ++res.sleepset_blocked_trace_count;
+    }else if(assume_blocked){
+      ++res.assume_blocked_trace_count;
+    }else{
+      ++res.trace_count;
     }
     ++computation_count;
     if(t && t->has_errors() && !res.has_errors()){

src/ARM_test.cpp
@@ -2982,7 +2982,8 @@ declare i32 @pthread_create(i64*,%attr_t*,i8*(i8*)*,i8*)
   DPORDriver::Result res = driver->run();
   delete driver;
   BOOST_CHECK(!res.has_errors());
-  BOOST_CHECK(res.trace_count == 3);
+  BOOST_CHECK_EQUAL(res.trace_count, 2);
+  BOOST_CHECK_EQUAL(res.assume_blocked_trace_count, 1);
 }
 
 BOOST_AUTO_TEST_CASE(LB_assume_2){
@@ -3015,7 +3016,8 @@ declare i32 @pthread_create(i64*,%attr_t*,i8*(i8*)*,i8*)
   DPORDriver::Result res = driver->run();
   delete driver;
   BOOST_CHECK(!res.has_errors());
-  BOOST_CHECK(res.trace_count == 4);
+  BOOST_CHECK_EQUAL(res.trace_count, 4);
+  BOOST_CHECK_EQUAL(res.assume_blocked_trace_count, 0);
 }
 
 BOOST_AUTO_TEST_CASE(LB_assume_3){
@@ -3048,7 +3050,8 @@ declare i32 @pthread_create(i64*,%attr_t*,i8*(i8*)*,i8*)
   DPORDriver::Result res = driver->run();
   delete driver;
   BOOST_CHECK(!res.has_errors());
-  BOOST_CHECK(res.trace_count == 1);
+  BOOST_CHECK_EQUAL(res.trace_count, 0);
+  BOOST_CHECK_EQUAL(res.assume_blocked_trace_count, 1);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

src/DPORDriver.h
@@ -25,6 +25,7 @@
 #include "Configuration.h"
 #include "Trace.h"
 #include "TraceBuilder.h"
+#include "DPORInterpreter.h"
 
 #if defined(HAVE_LLVM_IR_MODULE_H)
 #include <llvm/IR/Module.h>
@@ -63,7 +64,8 @@ public:
   class Result{
   public:
     /* Empty result */
-    Result() : trace_count(0), sleepset_blocked_trace_count(0), error_trace(0) {};
+    Result() : trace_count(0), sleepset_blocked_trace_count(0),
+               assume_blocked_trace_count(0), error_trace(0) {};
     ~Result(){
       if(all_traces.empty()){ // Otherwise error_trace also appears in all_traces.
         delete error_trace;
@@ -76,6 +78,8 @@ public:
     uint64_t trace_count;
     /* The number of explored sleepset-blocked traces */
     uint64_t sleepset_blocked_trace_count;
+    /* The number of explored assume-blocked traces */
+    uint64_t assume_blocked_trace_count;
     /* An empty trace if no error has been encountered. Otherwise some
      * error trace.
      */
@@ -108,7 +112,7 @@ private:
   std::string src;
 
   DPORDriver(const Configuration &conf);
-  Trace *run_once(TraceBuilder &TB) const;
+  Trace *run_once(TraceBuilder &TB, bool &assume_blocked) const;
   void reparse();
   /* Opens and reads the file filename. Stores the entire content in
    * tgt. Throws an exception on failure.
@@ -118,7 +122,8 @@ private:
    * the TraceBuilder TB. The kind of execution engine is determined
    * by conf.memory_model.
    */
-  llvm::ExecutionEngine *create_execution_engine(TraceBuilder &TB, const Configuration &conf) const;
+  std::unique_ptr<DPORInterpreter>
+  create_execution_engine(TraceBuilder &TB, const Configuration &conf) const;
 };
 
 #endif

src/DPORDriver_test.cpp
@@ -147,10 +147,10 @@ namespace DPORDriver_test {
         retval = false;
       }
     }
-    if(res.trace_count != int(spec.size())){
+    if((res.trace_count + res.assume_blocked_trace_count) != spec.size()){
       llvm::dbgs() << "DPORDriver_test::check_all_traces: expected number of traces: "
                    << spec.size() << ", actual number: "
-                   << res.trace_count
+                   << res.trace_count << "+" << res.assume_blocked_trace_count
                    << " (" << res.all_traces.size() << ")\n";
       retval = false;
     }

src/DPORInterpreter.h
@@ -1,0 +1,47 @@
+/* Copyright (C) 2019 Magnus Lång
+ *
+ * This file is part of Nidhugg.
+ *
+ * Nidhugg is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nidhugg is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+
+#ifndef __DPOR_INTERPRETER_H__
+#define __DPOR_INTERPRETER_H__
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+
+/* Common base class for all Interpreter instances used in nidhugg */
+class DPORInterpreter : public llvm::ExecutionEngine {
+  /* True if we have executed a false assume statement.
+   */
+  bool AssumeBlocked;
+protected:
+  void setAssumeBlocked(bool value) { AssumeBlocked = value; }
+public:
+  DPORInterpreter(llvm::Module *M)
+#ifdef LLVM_EXECUTIONENGINE_MODULE_UNIQUE_PTR
+  : llvm::ExecutionEngine(std::unique_ptr<llvm::Module>(M))
+#else
+  : llvm::ExecutionEngine(M)
+#endif
+  {
+    AssumeBlocked = false;
+  }
+  bool assumeBlocked() const { return AssumeBlocked; }
+};
+
+#endif

src/Execution.cpp
@@ -3071,6 +3071,7 @@ void Interpreter::callAssume(Function *F, const std::vector<GenericValue> &ArgVa
       }
       return;
     }
+    setAssumeBlocked(true);
     ECStack()->clear();
     AtExitHandlers.clear();
     Threads[CurrentThread].AssumeBlocked = true;

src/Interpreter.cpp
@@ -60,8 +60,9 @@ using namespace llvm;
 
 /// create - Create a new interpreter object.  This can never fail.
 ///
-ExecutionEngine *Interpreter::create(Module *M, TSOPSOTraceBuilder &TB,
-                                     const Configuration &C, std::string* ErrStr) {
+std::unique_ptr<Interpreter> Interpreter::
+create(Module *M, TSOPSOTraceBuilder &TB, const Configuration &C,
+       std::string* ErrStr) {
   // Tell this Module to materialize everything and release the GVMaterializer.
 #ifdef LLVM_MODULE_MATERIALIZE_ALL_PERMANENTLY_ERRORCODE_BOOL
   if(std::error_code EC = M->materializeAllPermanently()){
@@ -93,7 +94,7 @@ ExecutionEngine *Interpreter::create(Module *M, TSOPSOTraceBuilder &TB,
   }
 #endif
 
-  return new Interpreter(M,TB,C);
+  return std::unique_ptr<Interpreter>(new Interpreter(M,TB,C));
 }
 
 //===----------------------------------------------------------------------===//
@@ -101,12 +102,7 @@ ExecutionEngine *Interpreter::create(Module *M, TSOPSOTraceBuilder &TB,
 //
 Interpreter::Interpreter(Module *M, TSOPSOTraceBuilder &TB,
                          const Configuration &C)
-#ifdef LLVM_EXECUTIONENGINE_MODULE_UNIQUE_PTR
-  : ExecutionEngine(std::unique_ptr<Module>(M)),
-#else
-  : ExecutionEngine(M),
-#endif
-    TD(M), TB(TB), conf(C) {
+  : DPORInterpreter(M), TD(M), TB(TB), conf(C) {
 
   Threads.push_back(Thread());
   Threads.back().cpid = CPid();

src/Interpreter.h
@@ -43,6 +43,7 @@
 #include "VClock.h"
 #include "Option.h"
 #include "TSOPSOTraceBuilder.h"
+#include "DPORInterpreter.h"
 
 #include <llvm/ExecutionEngine/ExecutionEngine.h>
 #include <llvm/ExecutionEngine/GenericValue.h>
@@ -99,7 +100,7 @@ struct ExecutionContext {
 
 // Interpreter - This class represents the entirety of the interpreter.
 //
-class Interpreter : public ExecutionEngine, public InstVisitor<Interpreter> {
+class Interpreter : public DPORInterpreter, public InstVisitor<Interpreter> {
 protected:
   GenericValue ExitValue;          // The return value of the called function
   DataLayout TD;
@@ -265,9 +266,10 @@ public:
 
   /// create - Create an interpreter ExecutionEngine. This can never fail.
   ///
-  static ExecutionEngine *create(Module *M, TSOPSOTraceBuilder &TB,
-                                 const Configuration &conf = Configuration::default_conf,
-                                 std::string *ErrorStr = 0);
+  static std::unique_ptr<Interpreter>
+  create(Module *M, TSOPSOTraceBuilder &TB,
+         const Configuration &conf = Configuration::default_conf,
+         std::string *ErrorStr = 0);
 
   /// run - Start execution with the specified function and arguments.
   ///

commit dc1cb3ed65197eeddac0a816d05ebf36fdaae3dd

Fix uninitialised variable in DPORDriver::run()
Fixes: 22cd13b0: Separately count executions aborted by assume(false)

src/DPORDriver.cpp
@@ -250,7 +250,8 @@ DPORDriver::Result DPORDriver::run(){
     if((computation_count+1) % 1000 == 0){
       reparse();
     }
-    bool t_used, assume_blocked = false;
+    bool t_used = false;
+    bool assume_blocked = false;
     Trace *t = run_once(*TB, assume_blocked);
     if(t && conf.debug_collect_all_traces){
       res.all_traces.push_back(t);