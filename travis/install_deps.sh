8e1c742dd3d5cb8776ac05d322c1640a1a4ba3cf
Magnus Lång <magnus.lang@it.uu.se>
2019-10-10 17:56:33 +0200

    Travis: Run LLVM 6.0 from native Ubuntu Bionic package

--- travis/install_deps.sh
+++ travis/install_deps.sh
@@ -2,57 +2,59 @@
 mkdir -p cache
 
 # LLVM
-LLVM_TRIPLE=x86_64-linux-gnu
-LLVM_OS=ubuntu
-LLVM_REL_EXT=tar.xz
+if [ -n "$LLVM_VERSION" ]; then
+    LLVM_TRIPLE=x86_64-linux-gnu
+    LLVM_OS=ubuntu
+    LLVM_REL_EXT=tar.xz
 
-case $LLVM_VERSION in
-    3.[89]*|[4-6].*)
-        LLVM_UBUNTU_VER=16.04
-        ;;
-    ?*)
-        LLVM_UBUNTU_VER=18.04
-        ;;
-esac
-LLVM_URL="http://releases.llvm.org/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE-$LLVM_OS-$LLVM_UBUNTU_VER.$LLVM_REL_EXT"
-LLVM_DEP="cache/$LLVM_VERSION.$LLVM_REL_EXT"
-LLVM_DIR="cache/clang+llvm-$LLVM_VERSION"
-
-echo $LLVM_URL
-echo $LLVM_DEP
-echo $LLVM_DIR
-
-# download and install
-if [ ! -f $LLVM_DEP ] ; then
-    echo "Downloading LLVM"
-    wget $LLVM_URL -O $LLVM_DEP
-    if [ ! $? -eq 0 ]; then
-	echo "Error: Download failed"
-	rm $LLVM_DEP || true
-	exit 1
-    fi
-else
-    echo "Found Cached Archive"
-fi
-
-if [ ! -d $LLVM_DIR ] ; then
-    echo "Extracting LLVM"
-    tar -xf $LLVM_DEP -C cache/
-    if [ ! $? -eq 0 ]; then
-	echo "Error: Extraction Failed"
-	rm $LLVM_DEP || true
-	exit 1
-    fi
     case $LLVM_VERSION in
-        3.5.*)
-	    mv cache/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE cache/clang+llvm-$LLVM_VERSION
-	    ;;
+        3.[89]*|[4-6].*)
+            LLVM_UBUNTU_VER=16.04
+            ;;
         ?*)
-            mv cache/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE-$LLVM_OS-$LLVM_UBUNTU_VER cache/clang+llvm-$LLVM_VERSION
-	    ;;
+            LLVM_UBUNTU_VER=18.04
+            ;;
     esac
-else
-    echo "Found Cached Installation"
+    LLVM_URL="http://releases.llvm.org/$LLVM_VERSION/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE-$LLVM_OS-$LLVM_UBUNTU_VER.$LLVM_REL_EXT"
+    LLVM_DEP="cache/$LLVM_VERSION.$LLVM_REL_EXT"
+    LLVM_DIR="cache/clang+llvm-$LLVM_VERSION"
+
+    echo $LLVM_URL
+    echo $LLVM_DEP
+    echo $LLVM_DIR
+
+    # download and install
+    if [ ! -f $LLVM_DEP ] ; then
+        echo "Downloading LLVM"
+        wget $LLVM_URL -O $LLVM_DEP
+        if [ ! $? -eq 0 ]; then
+	    echo "Error: Download failed"
+	    rm $LLVM_DEP || true
+	    exit 1
+        fi
+    else
+        echo "Found Cached Archive"
+    fi
+
+    if [ ! -d $LLVM_DIR ] ; then
+        echo "Extracting LLVM"
+        tar -xf $LLVM_DEP -C cache/
+        if [ ! $? -eq 0 ]; then
+	    echo "Error: Extraction Failed"
+	    rm $LLVM_DEP || true
+	    exit 1
+        fi
+        case $LLVM_VERSION in
+            3.5.*)
+	        mv cache/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE cache/clang+llvm-$LLVM_VERSION
+	        ;;
+            ?*)
+                mv cache/clang+llvm-$LLVM_VERSION-$LLVM_TRIPLE-$LLVM_OS-$LLVM_UBUNTU_VER cache/clang+llvm-$LLVM_VERSION
+	        ;;
+        esac
+    else
+        echo "Found Cached Installation"
+    fi
 fi
 
 # Boost
