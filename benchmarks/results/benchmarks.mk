6af46b8448fb0f48300c43049317eb09caea03f2
Magnus Lång <magnus.lang@it.uu.se>
2020-05-15 13:21:19 +0200

    Benchmark config for paper

--- benchmarks/results/benchmarks.mk
+++ benchmarks/results/benchmarks.mk
@@ -1,23 +1,26 @@
 SHELL = /bin/bash -o pipefail
 
-TIME_LIMIT ?= 60 # seconds
+TIME_LIMIT ?= 36000 # seconds
 STACK_LIMIT ?= 65536 # kB
-MEM_LIMIT ?= 8388608 # kB
+MEM_LIMIT ?= 33554432 # kB
 
 SRCDIR = ../../..
 CLANG = clang
 CC = gcc
-OPT =
-CLANGFLAGS = -c -emit-llvm $(OPT)
+OPT = # -O1
+TOOLCLANGFLAGS = $(OPT)
+OPTFLAGS = -mem2reg -deadargelim
+CLANGFLAGS = -c -emit-llvm -Xclang -disable-O0-optnone $(TOOLCLANGFLAGS)
 NIDHUGG ?= nidhugg
 SOURCE    = $(NIDHUGG) -c11 -sc -source
 OPTIMAL   = $(NIDHUGG) -c11 -sc -optimal
 OBSERVERS = $(NIDHUGG) -c11 -sc -observers
 RFSC      = $(NIDHUGG) -c11 -sc -rf --n-threads=$(1)
 DCDPOR ?= dcdpor -dc
+VCDPOR ?= vcdpor -vc
 RCMC ?= rcmc
 WRCMC ?= $(RCMC) --wrc11
-GENMC ?= genmc
+GENMC ?= genmc -input-from-bitcode-file
 CDSC_DIR ?= /opt/cdschecker
 TIME = env time -f 'real %e\nres %M'
 TIMEOUT = timeout $(TIME_LIMIT)
@@ -25,14 +28,17 @@ ULIMIT = ulimit -Ss $(STACK_LIMIT) && ulimit -Sv $(MEM_LIMIT) &&
 RUN = -$(ULIMIT) $(TIMEOUT) $(TIME)
 TABULATE = ../../tabulate.sh
 
-TOOLS = genmc rfsc
-# ifneq ($(wildcard $(CDSC_DIR)/.),)
-#         TOOLS += cdsc
-# else
-# 	NATOOLS += cdsc
-# endif
+TOOLS = genmc rfsc rfsc32 vcdpor
+ifneq ($(wildcard $(CDSC_DIR)/.),)
+        TOOLS += cdsc
+else
+	NATOOLS += cdsc
+endif
+# SPECIAL_TOOLS are tools that need custom measurement rules, the default ones
+# will not be generated
+SPECIAL_TOOLS = cdsc rfsc32
 THREADS = 1
-rfsc_THREADS = 1 2 4 8 16 24 32 64 # 48 96
+rfsc_THREADS = 1 2 4 8 16 24 32 48 64 # 96
 # TOOLS=rfsc
 # THREADS = 1 2 4 8 16 24 32 48 64
 
@@ -47,7 +53,7 @@ BITCODE_FILES = $(N:%=code_%.bc)
 all: $(TABLES) $(BITCODE_FILES)
 
 code_%.bc: $(SRCDIR)/$(FILE)
-	$(CLANG) -DN=$* $(CLANGFLAGS) -o $@ $<
+	$(CLANG) -DN=$* $(CLANGFLAGS) -o - $< | opt $(OPTFLAGS) -o $@
 
 cdsc_%.elf: $(SRCDIR)/$(FILE)
 	$(CC) -Wl,-rpath=$(CDSC_DIR) $(OPT) -I../../cdsc_include \
@@ -56,23 +62,34 @@ cdsc_%.elf: $(SRCDIR)/$(FILE)
 		-DCDSC=1 -Dmain=user_main -pthread -DN=$* -o $@ $<
 
 define tool_template =
- $(1)_%_$(2).txt: code_%.bc
+ $(1)_$(3)_$(2).txt: code_$(3).bc
 	@date
 	$$(RUN) $$(call $(shell echo $(1) | tr a-z A-Z),$(2)) $$< 2>&1 | tee $$@
- $(1).txt: $$(foreach X,$$(N),$(1)_$$(X)_$(2).txt)
+ $(1).txt: $(1)_$(3)_$(2).txt
 endef
 
-$(foreach tool,$(filter-out cdsc,$(TOOLS)),\
+$(foreach tool,$(filter-out $(SPECIAL_TOOLS),$(TOOLS)),\
 	$(foreach t,$(or $($(tool)_THREADS), $(THREADS)),\
-		$(eval $(call tool_template,$(tool),$(t)))))
+		$(foreach n,$(N),\
+			$(eval $(call tool_template,$(tool),$(t),$(n))))))
 
 cdsc_%_1.txt: cdsc_%.elf
 	@date
 	$(RUN) ./$< 2>&1 | tee $@
+cdsc.txt: $(foreach n,$(N),cdsc_$(n)_1.txt)
 
-%.txt: $(TABULATE)
-	$(TABULATE) tool "$(NAME)" $* "$(N)" "$(or $($*_THREADS), $(THREADS))" \
-	  | column -t > $@
+rfsc32_%_1.txt: rfsc_%_32.txt
+	cp $< $@
+rfsc32.txt: $(foreach n,$(N),rfsc32_$(n)_1.txt)
+
+define tool_tab_template =
+$(1).txt: $$(TABULATE)
+	$$(TABULATE) tool "$$(NAME)" $(1) "$$(N)" "$$(or $$($(1)_THREADS), $$(THREADS))" \
+	  | column -t > $(1).txt
+endef
+
+$(foreach tool,$(TOOLS),\
+	$(eval $(call tool_tab_template,$(tool))))
 
 wide.txt: $(ALL_RESULTS) $(TABULATE)
 	$(TABULATE) wide "$(NAME)" "$(TOOLS)" "$(N)" "$(THREADS)" "$(NATOOLS)" \
