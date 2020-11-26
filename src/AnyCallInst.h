nidhugg/nidhugg

commit 3e22b3606bb82f91a60015dba85738b75c6987ad

Support try/catch around pthread_join
And pthread_mutex_lock

This affects the frontend for SC, TSO and PSO.

Fixes #84

src/AnyCallInst.h
@@ -43,6 +43,7 @@ class AnyCallInst {
   AnyCallInst(llvm::CallBase *CB) : CB(CB) {}
   AnyCallInst(llvm::CallBase &CB) : CB(&CB) {}
 
+  operator bool() const { return (bool)CB; }
   bool operator==(std::nullptr_t) { return !CB; }
   llvm::Instruction* operator &() { return CB; };
   const llvm::Instruction* operator &() const { return CB; };
@@ -62,6 +63,7 @@ class AnyCallInst {
   AnyCallInst() {}
   AnyCallInst(llvm::CallSite CS) : CS(CS) {}
 
+  operator bool() const { return (bool)CS; }
   bool operator==(std::nullptr_t) { return !CS.getInstruction(); }
   llvm::Instruction* operator &() { return CS.getInstruction(); };
   const llvm::Instruction* operator &() const { return CS.getInstruction(); };

src/Execution.cpp
@@ -3160,9 +3160,17 @@ bool Interpreter::isUnknownIntrinsic(Instruction &I){
   return false;
 }
 
+static AnyCallInst isCallOrInvoke(Instruction &I) {
+  if (CallInst *CI = dyn_cast<CallInst>(&I)) {
+    return {CI};
+  } else {
+    return {dyn_cast<InvokeInst>(&I)};
+  }
+}
+
 bool Interpreter::isPthreadJoin(Instruction &I, int *tid){
-  if(!isa<CallInst>(I)) return false;
-  AnyCallInst CI(static_cast<CallInst*>(&I));
+  AnyCallInst CI = isCallOrInvoke(I);
+  if(!CI) return false;
   Function *F = CI.getCalledFunction();
   if(!F || F->getName() != "pthread_join") return false;
   llvm::GenericValue gv_tid =
@@ -3176,8 +3184,8 @@ bool Interpreter::isPthreadMutexLock(Instruction &I, GenericValue **ptr){
     *ptr = (llvm::GenericValue*)Threads[CurrentThread].pending_mutex_lock;
     return true;
   }
-  if(!isa<CallInst>(I)) return false;
-  AnyCallInst CI(static_cast<CallInst*>(&I));
+  AnyCallInst CI = isCallOrInvoke(I);
+  if(!CI) return false;
   Function *F = CI.getCalledFunction();
   if(!F || F->getName() != "pthread_mutex_lock") return false;
   *ptr = (GenericValue*)GVTOP(getOperandValue(*CI.arg_begin(),ECStack()->back()));

src/SC_test2.cpp
@@ -609,6 +609,54 @@ declare void @__assert_fail()
   BOOST_CHECK(!res.has_errors());
 }
 
+BOOST_AUTO_TEST_CASE(pthread_join_invoke){
+  Configuration conf = DPORDriver_test::get_sc_conf();
+  DPORDriver *driver =
+    DPORDriver::parseIR(StrModule::portasm(R"(
+@x = global i32 0, align 4
+@t = global i8* null, align 4
+
+define i8* @p(i8*){
+  store i32 1, i32* @x, align 4
+  ret i8* null
+}
+
+define i32 @main(){
+  call i32 @pthread_create(i8** @t, %attr_t* null, i8*(i8*)* @p, i8* null)
+  %tid = load i8*, i8** @t, align 4
+  invoke i32 @pthread_join(i8* %tid, i8** null) to label %cont unwind label %catch
+
+cont:
+  %xv = load i32, i32* @x, align 4
+  %comp = icmp eq i32 %xv, 1
+  br i1 %comp, label %ok, label %err
+
+ok:
+  ret i32 0
+
+err:
+  call void @__assert_fail()
+  unreachable
+
+catch:
+  landingpad i8 cleanup
+  call void @__assert_fail()
+  unreachable
+}
+
+%attr_t = type {i64, [48 x i8]}
+declare i32 @pthread_create(i8**,%attr_t*,i8*(i8*)*,i8*)
+declare i32 @pthread_join(i8*,i8**)
+declare void @__assert_fail()
+)"),conf);
+
+  DPORDriver::Result res = driver->run();
+  delete driver;
+
+  BOOST_CHECK(res.trace_count == 1);
+  BOOST_CHECK(!res.has_errors());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif

commit 9805b5280dbb82947399f1c1b63fb2395d75b99a

Execution: Fix assertion failure on older llvms
llvm::CallSite may not be constructed from a null pointer, but our
AnyCallInst accepted it fine when using newer llvms, so this bug was
missed. Add an assertion to AnyCallInst so that we do not make the same
mistake again.

Fixes: 3e22b3606

src/AnyCallInst.h
@@ -27,6 +27,8 @@
 #include <llvm/IR/CallSite.h>
 #endif
 
+#include <cassert>
+
 /* Because older llvms do not have the llvm::CallBase base class, but instead
  * have a helper type llvm::CallSite that wraps any instruction type that is a
  * call, we need a consistent way of handling them. We introduce our own wrapper
@@ -40,7 +42,7 @@ class AnyCallInst {
 
  public:
   AnyCallInst() : CB(nullptr) {}
-  AnyCallInst(llvm::CallBase *CB) : CB(CB) {}
+  AnyCallInst(llvm::CallBase *CB) : CB(CB) { assert(CB); }
   AnyCallInst(llvm::CallBase &CB) : CB(&CB) {}
 
   operator bool() const { return (bool)CB; }

src/Execution.cpp
@@ -3217,8 +3217,10 @@ bool Interpreter::isUnknownIntrinsic(Instruction &I){
 static AnyCallInst isCallOrInvoke(Instruction &I) {
   if (CallInst *CI = dyn_cast<CallInst>(&I)) {
     return {CI};
+  } else if(InvokeInst *II = dyn_cast<InvokeInst>(&I)) {
+    return {II};
   } else {
-    return {dyn_cast<InvokeInst>(&I)};
+    return AnyCallInst();
   }
 }
 