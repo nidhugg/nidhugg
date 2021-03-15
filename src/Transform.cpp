07d80c4b6573c546afa0bd6f84018e6a1c2cd77c
Magnus Lång <magnus.lang@it.uu.se>
2021-03-15 12:27:10 +0100

    Workaround: Run unroller before optimisation
    The unroller is currently not safe to run after SSA transformation, so
    we schedule it first.

--- src/Transform.cpp
+++ src/Transform.cpp
@@ -98,6 +98,17 @@ namespace Transform {
     using PassManager = llvm::PassManager;
 #endif
     PassManager PM;
+    /* The unroller is currently not safe to run after SSA
+     * transformation, so we run it before everything else. spin-assume
+     * should come before the unroller, so we also run it here if loop
+     * unrolling is enabled.
+     */
+    if(conf.transform_loop_unroll >= 0){
+      if(conf.transform_spin_assume) {
+        PM.add(new SpinAssumePass());
+      }
+      PM.add(new LoopUnrollPass(conf.transform_loop_unroll));
+    }
     /* Run some safe simplifications that both improve applicability
      * of our passes, and speed up model checking.
      * We need to clear the "optnone" attribute set by clang, or all the
@@ -105,11 +116,8 @@ namespace Transform {
      */
     PM.add(new ClearOptnonePass());
     PM.add(llvm::createPromoteMemoryToRegisterPass());
-    if(conf.transform_spin_assume){
+    if(conf.transform_spin_assume && conf.transform_loop_unroll < 0){
       PM.add(new SpinAssumePass());
-    }
-    if(0 <= conf.transform_loop_unroll){
-      PM.add(new LoopUnrollPass(conf.transform_loop_unroll));
     }
     PM.add(new AddLibPass());
     bool modified = PM.run(mod);
