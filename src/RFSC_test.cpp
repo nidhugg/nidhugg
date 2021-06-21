895f4e695fce4eba3b0ffd27f41927d562b88fd5
Magnus Lång <margnus1@telia.com>
2021-06-21 18:57:08 +0200

    rf: Add test for multithreaded search pruning

--- src/RFSC_test.cpp
+++ src/RFSC_test.cpp
@@ -54,6 +54,56 @@ declare i32 @pthread_cond_init(i32*,i32*) nounwind
                    ("not supported:") , std::string::npos);
 }
 
+BOOST_AUTO_TEST_CASE(Threads_prune) {
+  Configuration conf = DPORDriver_test::get_sc_conf();
+  conf.dpor_algorithm = Configuration::READS_FROM;
+  conf.n_threads = 4;
+  conf.explore_all_traces = false;
+  std::string module = StrModule::portasm(R"(
+@x = global i32 0, align 4
+
+define i8* @p(i8* %arg){
+  %old = load atomic i32, i32* @x seq_cst, align 4
+  %inc = add i32 %old, 1
+  store atomic i32 %inc, i32* @x seq_cst, align 4
+
+  %old.1 = load atomic i32, i32* @x seq_cst, align 4
+  %inc.1 = add i32 %old.1, 1
+  store atomic i32 %inc.1, i32* @x seq_cst, align 4
+
+  %old.2 = load atomic i32, i32* @x seq_cst, align 4
+  %inc.2 = add i32 %old.2, 1
+  store atomic i32 %inc.2, i32* @x seq_cst, align 4
+
+  %bad = icmp sgt i32 %inc.2, 7
+  br i1 %bad, label %fail, label %succ
+
+fail:
+  call void @__assert_fail()
+  unreachable
+
+succ:
+  ret i8* null
+}
+
+define i32 @main(){
+  call i32 @pthread_create(i64* null, %attr_t* null, i8*(i8*)* @p, i8* null)
+  call i32 @pthread_create(i64* null, %attr_t* null, i8*(i8*)* @p, i8* null)
+  call i32 @pthread_create(i64* null, %attr_t* null, i8*(i8*)* @p, i8* null)
+  ret i32 0
+}
+%attr_t = type { i64, [48 x i8] }
+declare i32 @pthread_create(i64*, %attr_t*, i8*(i8*)*, i8*) nounwind
+declare void @__assert_fail() nounwind noreturn
+)");
+
+  std::unique_ptr<DPORDriver> driver(DPORDriver::parseIR(module, conf));
+  DPORDriver::Result res = driver->run();
+
+  BOOST_REQUIRE(res.has_errors());
+  BOOST_REQUIRE_EQUAL(res.error_trace->get_errors().size(), 1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif
