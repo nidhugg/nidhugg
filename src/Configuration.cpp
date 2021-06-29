nidhugg/nidhugg

commit ed33519fb563c89003386d8edeb66793b2616bd7

HACK: Make optimal work by ignoring the wakeup set

src/TSOTraceBuilder.cpp
@@ -1445,27 +1445,29 @@ void unordered_vector_delete(std::vector<T> &vec, std::size_t pos) {
 
 void
 TSOTraceBuilder::obs_sleep_wake(struct obs_sleep &sleep, const Event &e) const{
-  if (conf.dpor_algorithm != Configuration::OBSERVERS) {
-    if (e.wakeup.size()) {
-      for (unsigned i = 0; i < sleep.sleep.size();) {
-        if (e.wakeup.count(sleep.sleep[i].pid)) {
-          unordered_vector_delete(sleep.sleep, i);
-        } else {
-          ++i;
-        }
-      }
-    }
-  } else {
+  // if (conf.dpor_algorithm != Configuration::OBSERVERS) {
+  //   if (e.wakeup.size()) {
+  //     for (unsigned i = 0; i < sleep.sleep.size();) {
+  //       if (e.wakeup.count(sleep.sleep[i].pid)) {
+  //         unordered_vector_delete(sleep.sleep, i);
+  //       } else {
+  //         ++i;
+  //       }
+  //     }
+  //   }
+  // } else {
     sym_ty sym = e.sym;
-    /* A tricky part to this is that we must clear observers from the events
-     * we use to wake */
-    clear_observed(sym);
+    if (conf.dpor_algorithm == Configuration::OBSERVERS) {
+      /* A tricky part to this is that we must clear observers from the events
+       * we use to wake */
+      clear_observed(sym);
+    }
 #ifndef NDEBUG
     obs_wake_res res =
 #endif
       obs_sleep_wake(sleep, e.iid.get_pid(), sym);
     assert(res != obs_wake_res::BLOCK);
-  }
+  // }
 }
 
 static bool symev_does_load(const SymEv &e) {

commit c15b6c90f54f8e6c076c7319e35f2f291ea40297

TSOTraceBuilder: Fix breakage of TSO caused by commute-rmws
Fix the breakage of TSO by keeping the wakeup set for TSO and disabling
commute-rmws for all memory models except SC.

Fixes: ed33519f: HACK: Make optimal work by ignoring the wakeup set
Fixes: b6120d1f: Add command-line flag --commute-rmws

src/Configuration.cpp
@@ -221,7 +221,7 @@ void Configuration::assign_by_commandline(){
   print_progress = cl_print_progress || cl_print_progress_estimate;
   print_progress_estimate = cl_print_progress_estimate;
   debug_print_on_reset = cl_debug_print_on_reset;
-  commute_rmws = !cl_no_commute_rmws;
+  commute_rmws = !cl_no_commute_rmws && memory_model == SC;
   sat_solver = cl_sat;
   argv.resize(1);
   argv[0] = get_default_program_name();

src/Configuration.h
@@ -191,7 +191,7 @@ public:
   /* Number to return from __VERIFIER_nondet_u?int() */
   Option<int> svcomp_nondet_int;
   /* If set, rmws are allowed to commute. */
-  bool commute_rmws = true;
+  bool commute_rmws = false;
   /* If set, DPORDriver will continually print its progress to stdout. */
   bool print_progress;
   /* If set and print_progress is set, DPORDriver will together with

src/TSOTraceBuilder.cpp
@@ -1457,17 +1457,30 @@ void unordered_vector_delete(std::vector<T> &vec, std::size_t pos) {
 
 void
 TSOTraceBuilder::obs_sleep_wake(struct obs_sleep &sleep, const Event &e) const{
-  sym_ty sym = e.sym;
-  if (conf.dpor_algorithm == Configuration::OBSERVERS) {
-    /* A tricky part to this is that we must clear observers from the events
-     * we use to wake */
-    clear_observed(sym);
-  }
+  if (conf.memory_model == Configuration::TSO) {
+    assert(!conf.commute_rmws);
+    if (e.wakeup.size()) {
+      for (unsigned i = 0; i < sleep.sleep.size();) {
+        if (e.wakeup.count(sleep.sleep[i].pid)) {
+          unordered_vector_delete(sleep.sleep, i);
+        } else {
+          ++i;
+        }
+      }
+    }
+  } else {
+    sym_ty sym = e.sym;
+    if (conf.dpor_algorithm == Configuration::OBSERVERS) {
+      /* A tricky part to this is that we must clear observers from the events
+       * we use to wake */
+      clear_observed(sym);
+    }
 #ifndef NDEBUG
-  obs_wake_res res =
+    obs_wake_res res =
 #endif
-    obs_sleep_wake(sleep, e.iid.get_pid(), sym);
-  assert(res != obs_wake_res::BLOCK);
+      obs_sleep_wake(sleep, e.iid.get_pid(), sym);
+    assert(res != obs_wake_res::BLOCK);
+  }
 }
 
 static bool symev_does_load(const SymEv &e) {

src/DPORDriver_test.cpp
@@ -45,6 +45,7 @@ namespace DPORDriver_test {
     conf.memory_model = Configuration::SC;
     conf.debug_collect_all_traces = true;
     conf.explore_all_traces = true;
+    conf.commute_rmws = true;
     return conf;
   }
 